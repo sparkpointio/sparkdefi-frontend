0bcf3d8798435a40e5693794009923877fb7d02f
Mat <mativanarquero@gmail.com>
2021-08-17 10:46:25 +0800

    Add SRK Earned and adjust text formatting

--- src/views/Pools/components/PoolCard/CardActions/HarvestActions.tsx
+++ src/views/Pools/components/PoolCard/CardActions/HarvestActions.tsx
@@ -5,6 +5,7 @@ import { Token } from 'config/constants/types'
 import { useTranslation } from 'contexts/Localization'
 import { getFullDisplayBalance, getBalanceNumber, formatNumber } from 'utils/formatBalance'
 import Balance from 'components/Balance'
+import styled from 'styled-components'
 import CollectModal from '../Modals/CollectModal'
 
 interface HarvestActionsProps {
@@ -47,6 +48,10 @@ const HarvestActions: React.FC<HarvestActionsProps> = ({
     />,
   )
 
+  const InlineText = styled(Text)`
+  display: inline;
+`
+
   return (
     <Flex flexDirection="column" mb="16px">
       <Flex justifyContent="space-between" alignItems="center">
@@ -58,7 +63,8 @@ const HarvestActions: React.FC<HarvestActionsProps> = ({
               {hasEarnings ? (
                 <Balance bold fontSize="20px" decimals={5} value={earningTokenBalance} />
               ) : (
-                <Heading color="textDisabled">0</Heading>
+                // <Heading color="textDisabled">0</Heading>
+                <Text>SRK Staked</Text>
               )}
               {earningTokenPrice !== 0 && (
                 <Text fontSize="12px" color={hasEarnings ? 'textSubtle' : 'textDisabled'}>
@@ -70,10 +76,41 @@ const HarvestActions: React.FC<HarvestActionsProps> = ({
                       color="textSubtle"
                       decimals={2}
                       value={earningTokenDollarBalance}
-                      unit=" USD"
+                      unit=" SRK"
                     />
                   ) : (
-                    '0 USD'
+                    '0 SRK'
+                  )}
+                </Text>
+              )}
+            </>
+          )}
+        </Flex>
+        <Flex marginRight="-12vh" flexDirection="column">
+          {isLoading ? (
+            <Skeleton width="80px" height="48px" />
+          ) : (
+            <>
+              {hasEarnings ? (
+                <Balance bold fontSize="20px" decimals={5} value={earningTokenBalance} />
+              ) : (
+                // <Heading color="textDisabled">0</Heading>
+                <Text>SRK Earned</Text>
+              )}
+              {earningTokenPrice !== 0 && (
+                <Text fontSize="12px" color={hasEarnings ? 'textSubtle' : 'textDisabled'}>
+                  ~
+                  {hasEarnings ? (
+                    <Balance
+                      display="inline"
+                      fontSize="12px"
+                      color="textSubtle"
+                      decimals={2}
+                      value={earningTokenDollarBalance}
+                      // unit=" USD"
+                    />
+                  ) : (
+                    '0.0000'
                   )}
                 </Text>
               )}
@@ -81,9 +118,20 @@ const HarvestActions: React.FC<HarvestActionsProps> = ({
           )}
         </Flex>
         <Flex>
+          {/*
           <Button disabled={!hasEarnings} onClick={onPresentCollect}>
-            {isCompoundPool ? t('Collect') : t('Harvest')}
+             {isCompoundPool ? t('Collect') : t('Harvest')} 
+             {isCompoundPool ? t('Collect') : ('SRK Earned')}
           </Button>
+          */}
+          {/*  
+          <InlineText color="secondary" textTransform="uppercase" bold fontSize="12px">
+                {`${earningToken.symbol} `}
+              </InlineText>
+              <InlineText color="textSubtle" textTransform="uppercase" bold fontSize="12px">
+                &nbsp;{t('Earned')}
+              </InlineText>
+          */}
         </Flex>
       </Flex>
     </Flex>
