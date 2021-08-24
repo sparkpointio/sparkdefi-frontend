c1fd7bdd65d2bedb82670decd39f0893352c9efc
Aldrick Bonaobra <git@aldrickb.com>
2021-08-24 22:19:54 +0800

    Add data to UI

--- src/views/Pools/components/PoolCard/Modals/StakeModal.tsx
+++ src/views/Pools/components/PoolCard/Modals/StakeModal.tsx
@@ -10,7 +10,9 @@ import { ChevronDown, ChevronUp } from 'react-feather';
 import useTheme from 'hooks/useTheme'
 import useToast from 'hooks/useToast'
 import BigNumber from 'bignumber.js'
-import { getFullDisplayBalance, formatNumber, getDecimalAmount } from 'utils/formatBalance'
+import { getFullDisplayBalance, formatNumber, getDecimalAmount, getBalanceNumber } from 'utils/formatBalance'
+import { BIG_ZERO } from 'utils/bigNumber'
+import useTokenBalance from 'hooks/useTokenBalance'
 import { Pool } from 'state/types'
 import { getAddress } from 'utils/addressHelpers'
 import StakeTokenModal from './Stake';
@@ -64,6 +66,17 @@ const StakeModal: React.FC<StakeModalProps> = ({
   
   const usdValueStaked = stakeAmount && formatNumber(new BigNumber(stakeAmount).times(stakingTokenPrice).toNumber())
   const [activeSelect, setActiveSelect] = useState(false)
+  
+  const { balance: earnedTokenBalance } = useTokenBalance(pool.earningToken.address[56])
+
+  const totalStakingTokens = userData?.stakingTokenBalance ? getBalanceNumber(new BigNumber(userData.stakingTokenBalance), stakingToken.decimals) : BIG_ZERO
+  const totalStakedTokens = userData?.stakedBalance ? getBalanceNumber(new BigNumber(userData.stakedBalance), stakingToken.decimals) : BIG_ZERO
+  const totalEarningTokens = earnedTokenBalance ? getBalanceNumber(new BigNumber(earnedTokenBalance)) : BIG_ZERO
+  const totalEarnedTokens = userData?.pendingReward ? getBalanceNumber(new BigNumber(userData.pendingReward)) : BIG_ZERO
+
+  const temp = new BigNumber(pool.tokenPerBlock).times( new BigNumber(userData.stakedBalance).div(pool.totalStaked)  ) 
+
+  const rewardRate = pool?.tokenPerBlock ? getBalanceNumber(temp) : BIG_ZERO
   
   useEffect(() => {
     if (stakingLimit.gt(0) && !isRemovingStake) {
@@ -143,18 +156,18 @@ const StakeModal: React.FC<StakeModalProps> = ({
         <Text fontSize="15px" marginLeft="10px">Staking, balances & earnings</Text>
         <StyledFlex marginTop="21px">
           <Flex flexDirection="column">
-            <Text fontSize="24px">0.0000</Text>
+            <Text fontSize="24px">{totalStakingTokens.toFixed(4)}</Text>
             <Text color="textSubtle">{pool.stakingToken.symbol} Tokens</Text>
             <Button fullWidth>Add more</Button>
           </Flex>
           <Flex flexDirection="column">
-            <Text fontSize="24px">0.0000</Text>
+            <Text fontSize="24px">{totalEarningTokens.toFixed(4)}</Text>
             <Text color="textSubtle">{pool.earningToken.symbol} Tokens</Text>
             <Button fullWidth>Add More</Button>
           </Flex>
           <Flex flexDirection="column">
-            <Text fontSize="24px">0.0000</Text>
-            <Text color="textSubtle">{pool.stakingToken.symbol} Tokens</Text>
+            <Text fontSize="24px">{totalStakedTokens.toFixed(4)}</Text>
+            <Text color="textSubtle">{pool.stakingToken.symbol} Staked</Text>
             <Button fullWidth onClick={onPresentStakeAction}>Stake Tokens</Button>
           </Flex>
         </StyledFlex>
@@ -163,11 +176,11 @@ const StakeModal: React.FC<StakeModalProps> = ({
         </StyledFlex>
         <StyledFlex marginTop="30px" marginBottom="20px">
           <Flex flexDirection="column">
-            <Text fontSize="24px">0.0000</Text>
-            <Text color="textSubtle" fontSize="17px">Your Rate {pool.earningToken.symbol}/Week Tokens</Text>
+            <Text fontSize="24px">{rewardRate.toFixed(4)}</Text>
+            <Text color="textSubtle" fontSize="17px">Your Rate {pool.earningToken.symbol}/block Tokens</Text>
           </Flex>
           <Flex flexDirection="column">
-            <Text fontSize="24px">0.0000</Text>
+            <Text fontSize="24px">{totalEarnedTokens.toFixed(4)}</Text>
             <Text color="textSubtle" fontSize="17px">{pool.earningToken.symbol} Token Earnings</Text>
           </Flex>
           <Flex flexDirection="column" mb="16px" marginLeft="5px"
--- src/views/Pools/components/PoolCard/index.tsx
+++ src/views/Pools/components/PoolCard/index.tsx
@@ -1,12 +1,15 @@
 import BigNumber from 'bignumber.js'
 import React, { useContext } from 'react'
-import { CardBody, Flex, Text } from '@sparkpointio/sparkswap-uikit'
+import { CardBody, Flex, Text , Link, LinkExternal} from '@sparkpointio/sparkswap-uikit'
 import { ThemeContext } from 'styled-components'
 import UnlockButton from 'components/UnlockButton'
 import { useTranslation } from 'contexts/Localization'
 import { BIG_ZERO } from 'utils/bigNumber'
 import { Pool } from 'state/types'
-
+import { getBalanceNumber } from 'utils/formatBalance'
+import { getPoolBlockInfo } from 'views/Pools/helpers'
+import { useBlock } from 'state/block/hooks'
+import { getBscScanLink } from 'utils'
 import AprRow from './AprRow'
 import { StyledCard, StyledCardInner } from './StyledCard'
 import CardFooter from './CardFooter'
@@ -15,11 +18,22 @@ import CardActions from './CardActions'
 
 
 const PoolCard: React.FC<{ pool: Pool; account: string }> = ({ pool, account }) => {
-  const { sousId, stakingToken, earningToken, isFinished, userData } = pool
+  const { sousId, stakingToken, earningToken, isFinished, userData, startBlock, endBlock } = pool
+  console.log(pool)
   const { t } = useTranslation()
   const stakedBalance = userData?.stakedBalance ? new BigNumber(userData.stakedBalance) : BIG_ZERO
   const accountHasStakedBalance = stakedBalance.gt(0)
   const theme = useContext(ThemeContext)
+
+  const totalStaked = pool.totalStaked ? getBalanceNumber(new BigNumber(pool.totalStaked.toString()), stakingToken.decimals) : BIG_ZERO
+
+  const rewardPerBlock = pool?.tokenPerBlock ? getBalanceNumber(new BigNumber(pool.tokenPerBlock.toString()), earningToken.decimals) : BIG_ZERO
+  
+  
+  const { currentBlock } = useBlock()
+  
+  const { shouldShowBlockCountdown, blocksUntilStart, blocksRemaining, hasPoolStarted, blocksToDisplay } =
+  getPoolBlockInfo(pool, currentBlock)
   
   return (
     <StyledCard
@@ -32,15 +46,22 @@ const PoolCard: React.FC<{ pool: Pool; account: string }> = ({ pool, account }) 
           isFinished={isFinished && sousId !== 0}
         />
          <hr style={{width: '100%', border: 'none', backgroundColor: theme.colors.primary, height: '2px'}}/>
-      
+
+          <Flex justifyContent="space-between" style={{textAlign: 'left'}}>
+            <Text>Remaining blocks</Text>
+            <Link external href={getBscScanLink(hasPoolStarted ? endBlock : startBlock, 'countdown')}>
+              <Text>{`${blocksRemaining}`} blocks</Text>
+            </Link>
+          </Flex>
+
           {/* <AprRow pool={pool} stakingTokenPrice={stakingTokenPrice} /> */}
           <Flex justifyContent="space-between" style={{textAlign: 'left'}}>
             <Text>Total Deposit</Text>
-            <Text>00.00</Text>
+            <Text>{`${totalStaked} ${stakingToken.symbol}`}</Text>
           </Flex>
           <Flex justifyContent="space-between" style={{textAlign: 'left'}}>
-            <Text>Pool Size</Text>
-            <Text>00.00</Text>
+              <Text>Reward per block</Text>
+              <Text>{rewardPerBlock}</Text>
           </Flex>
           <Flex justifyContent="space-between" style={{textAlign: 'left'}}>
             <Text>APY</Text>
