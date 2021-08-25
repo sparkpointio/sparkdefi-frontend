f42b82814e06b6655da21a47fda7822081b33e3e
Mat <mativanarquero@gmail.com>
2021-08-25 13:43:12 +0800

    Add and Update PoolCard modal styles

--- src/views/Pools/components/PoolCard/Modals/Styled.tsx
+++ src/views/Pools/components/PoolCard/Modals/Styled.tsx
@@ -2,12 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 import { Flex, Button } from '@sparkpointio/sparkswap-uikit' 
 
+// const Container = styled.div`
+//     margin-top: -30px;
+//     display: flex;
+//     flex-direction: column;
+//     min-width: 500px;
+// `
+
 const Container = styled.div`
-    margin-top: -30px;
+    min-width: 720px;
     display: flex;
-    flex-direction: column;
-    min-width: 500px;
+    justify-content: space-between;
+    height: auto;
 `
+
 const StyledFlex = styled(Flex)`
     padding: 10px 0px;
 `
@@ -20,6 +28,23 @@ const DepositButton = styled(Button)`
     margin-left: 10px;
 `
 
-export {StyledFlex, CancelButton, DepositButton}
+const ActionDiv = styled.div`
+    margin: 15px 0px;
+    padding: 10px;
+`
+
+const DetailsCont = styled.div`
+    width: 30%;
+    height: auto;
+    overflow: visible;
+`
+
+const ModalFooter = styled.div`
+    min-width: 720px;
+    display: flex;
+    justify-content: space-between;
+`
+
+export {StyledFlex, CancelButton, DepositButton, ActionDiv, DetailsCont, ModalFooter}
 export default Container;
 
