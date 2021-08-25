e808ff49e975e36d743246ee8a52ef7d3390a49d
Mat <mativanarquero@gmail.com>
2021-08-25 13:41:10 +0800

    Update divider for pools stake modal

---
+++ src/views/Pools/components/PoolCard/Modals/Divider.tsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import styled, { ThemeContext } from 'styled-components'
+
+export default styled.div`
+  background-color: ${({ theme }) => theme.colors.textSubtle};
+  height: 1px;
+  margin: 0 auto 32px;
+  width: 100%;
+`
+
+export const StyledHr = styled.div`
+  border-style: solid none none none;
+  border-width: 2px;
+  border-color: ${({theme}) => theme.colors.primary};
+  width: 95%;
+`
+
+export const ModalHr = styled.hr`
+  border-style: solid none none none;
+  border-width: 2px;
+  border-color: ${({theme}) => theme.colors.primary};
+  margin-top: 20px;
+  margin-bottom: 20px;
+  width: 100%;
+`
